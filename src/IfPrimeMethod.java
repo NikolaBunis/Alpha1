f1bdbb6f9f93d5ecf2dd573ffda48ecb037b1038
[NikolaBunis] <[bunisbg@gmail.com]>
2018-05-31 13:17:20 +0300

    Third commit of  my method that determines if a number is prime.

--- src/IfPrimeMethod.java
+++ src/IfPrimeMethod.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class IfPrimeMethod {
+
+    public static void main (String[] args){
+
+Scanner in = new Scanner(System.in);
+
+int N = in.nextInt();
+
+
+if (PrimeCheck(N)){
+
+    System.out.printf("The Number " + N +  " is prime");
+
+}
+
+else {
+
+    System.out.println("The Number " + N +  " is not prime");
+}
+
+
+
+    }
+
+    public static boolean PrimeCheck(int number){
+
+
+
+        int maxDivider = (int) Math.sqrt(number);
+        int divider = 2;
+        boolean primeCheck = true;
+
+
+        while (primeCheck && (divider <= maxDivider)) {
+            if (number % divider == 0) {
+                primeCheck = false;
+            }
+            divider++;
+
+
+
+        }
+        if (primeCheck) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+
+
 }
